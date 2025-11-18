9e2eb72ae76c9dfe9af7a14047a6b2904de3d65b
Marcos Cunha <marcos.cunha1025@gmail.com>
2025-11-18 20:18:00 -0300

    Created project Spring Boot

---
+++ src/main/java/br/com/marcoscunha/TemperoDoSertao/TemperoDoSertaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcoscunha.TemperoDoSertao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperoDoSertaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperoDoSertaoApplication.class, args);
+	}
+
+}
