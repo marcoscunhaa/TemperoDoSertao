b33389b2b5b69ec3e98963c689f896e039fdbc0b
Marcos Cunha <marcos.cunha1025@gmail.com>
2025-11-24 21:02:08 -0300

    Implementado gráficos, funções de troco e valor no peso

--- src/main/resources/static/dist/TemperoDoSertao/server/angular-app-manifest.mjs
+++ src/main/resources/static/dist/TemperoDoSertao/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5363, hash: '6b530d142b62b541a63e679256ced320f9de12963a4318636e80fa57c6952f0c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1335, hash: '41e54390d7cbcbf6c105b97381db1c00721662a942daf783a847119939af078f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 5410, hash: '74d10a3919758d97100019e124bef566c4a46fedad422ec72d82426beca109df', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5363, hash: '69e5d95f687aa1811276f1147db99b0a229eb30194452a40cd9a0dd35f839136', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1335, hash: '0f1b0a130aa4721ece87db98550b05c5625c5fa8a59538732359a7e624c8b0df', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 5410, hash: 'e329357e50f16793cc5033662ad3e11a15f59a578a159cd5762340291c03a7e2', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-HR6D4WJL.css': {size: 315730, hash: 'q9j6xvPSaNQ', text: () => import('./assets-chunks/styles-HR6D4WJL_css.mjs').then(m => m.default)}
   },
 };
